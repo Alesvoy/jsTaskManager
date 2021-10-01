c710f67cd183206071ef546bf5a2c7097f1bc30e
Alejandro Butron <alesvoy@gmail.com>
2021-09-30 21:08:48 -0500

    Add task functionality implemented

--- js/app.js
+++ js/app.js
@@ -1,1 +1,32 @@
-console.log("Hello World");
+const App = (() => {
+  // Caching the DOM
+  const inputEl = document.querySelector("input");
+  const ulEl = document.querySelector(".task__list");
+  const removeBtn = document.querySelector(".complete");
+
+  const eventListeners = () => {
+    inputEl.addEventListener("keypress", (e) => {
+      if (e.keyCode === 13) {
+        render();
+        inputEl.value = "";
+      }
+    });
+  };
+
+  const render = () => {
+    ulEl.innerHTML += `
+      <li class="task__item">
+        ${inputEl.value} <button class="complete">Complete</button>
+      </li>`;
+  };
+
+  const init = () => {
+    eventListeners();
+  };
+
+  return {
+    init: init,
+  };
+})();
+
+App.init();
